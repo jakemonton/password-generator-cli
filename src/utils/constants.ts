3cc789e4f082b7e25103d65f8d073efb8e68bcf0
Jake Monton <montonjake89@gmail.com>
2021-07-09 11:46:04 +1000

    Remove redundant letter

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const UPPERCASE_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-export const LOWERCASE_ALPHABET = 'abcdefghijgklmnopqrstuvwxyz';
+export const LOWERCASE_ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 export const NUMBERS = '0123456789';
 export const SYMBOLS = '!@#$%^&?+=_'
 export const AMBIGUOUS_SYMBOLS = '{}[]()/\\~\'\"`,;:.<>-*';
